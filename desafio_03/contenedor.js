c56810fd7669eec2af3aab984f4c76ec0c0b5fbf
ctorchia <cristian_torchia@yahoo.com.ar>
2022-07-24 18:02:05 -0300

    Desafio 03 - OK

---
+++ desafio_03/contenedor.js
@@ -0,0 +1,102 @@
+const fs = require('fs');
+
+class Producto {
+    constructor(ruta) {
+        this.ruta = ruta
+    }
+
+    // save(Object) : Number
+
+    async save(obj) {
+        try {
+
+            let dataArch = await fs.promises.readFile(this.ruta, 'utf8');
+            let dataArchParse = JSON.parse(dataArch);
+            if (dataArchParse.length) {
+
+                await fs.promises.writeFile(this.ruta, JSON.stringify([...dataArchParse, { ...obj, id: dataArchParse[dataArchParse.length - 1].id + 1 }], null, 2))
+                let idProduct = dataArchParse[dataArchParse.length - 1].id + 1
+                console.log(`El producto tiene el ID: ${idProduct}`);
+                return idProduct;
+
+            } else {
+
+                await fs.promises.writeFile(this.ruta, JSON.stringify([{ ...obj, id: 1 }], null, 2))
+                console.log(`El producto tiene el ID: 1`);
+                return 1;
+
+            }
+
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    // getByID(Number) : Object
+
+    async getById(id) {
+        try {
+            let dataArch = await fs.promises.readFile(this.ruta, 'utf8')
+            let dataArchParse = JSON.parse(dataArch)
+            let producto = dataArchParse.find(producto => producto.id === id)
+            if (producto) {
+                console.log(producto)
+                return producto
+            } else {
+                console.log('El producto no existe');
+                return null
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    // getAll() : Object[]
+
+    async getAll() {
+        try {
+            let dataArch = await fs.promises.readFile(this.ruta, 'utf8')
+            let dataArchParse = JSON.parse(dataArch)
+            if (dataArchParse.length) {
+                console.log(dataArchParse)
+                return dataArchParse
+
+            } else {
+                console.log('No hay Productos')
+            }
+
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    // deleteById(Number) : void
+
+    async deleteById(id) {
+        try {
+            let dataArch = await fs.promises.readFile(this.ruta, 'utf8')
+            let dataArchParse = JSON.parse(dataArch)
+            let producto = dataArchParse.find(producto => producto.id === id)
+            if (producto) {
+                let dataArchParseFiltered = dataArchParse.filter(producto => producto.id !== id)
+                await fs.promises.writeFile(this.ruta, JSON.stringify(dataArchParseFiltered, null, 2))
+                console.log('Producto Eliminado')
+            } else {
+                console.log('No se encontró el producto')
+            }
+
+        } catch (error) {
+            console.log(error);
+
+        }
+    }
+
+    // deleteAll() : void
+
+    async deleteAll() {
+        await fs.promises.writeFile(this.ruta, JSON.stringify([], null, 2), 'utf8')
+        console.log('Todos los productos se han eliminado')
+    }
+}
+
+module.exports = Producto;
