d2e6145e88d5863eb8d44010535abbeacaa2ff46
DESKTOP-V2BVTP7\CMT <cristian_torchia@yahoo.com.ar>
2022-10-19 16:45:54 -0300

    Desafio 14: First Commit

---
+++ desafio_14/src/routes/miscellaneous.routes.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const router = express.Router();
+const {arguments} = require('../config');
+const {fork} = require('child_process');
+const numCPUs = require('os').cpus().length
+
+router.get("/info", (req, res) => {
+
+    info = {
+        args : JSON.stringify(arguments),
+        path : process.execPath,
+        platform: process.platform,
+        processId : process.pid,
+        nodeVersion : process.version,
+        directoryProject : process.cwd(),
+        memory: JSON.stringify(process.memoryUsage()),
+        numCPUs: numCPUs
+    }
+
+    res.render('info',{info});
+});
+
+router.get("/api/randoms",(req,res)=>{
+    let {cant} = req.query;
+    console.log(cant);
+    const random = fork('../desafio_12/src/api/randoms.js',[cant]);
+    random.send('start');
+    random.on('message',obj=>{
+        res.json(obj);
+    })
+});
+
+module.exports = router
