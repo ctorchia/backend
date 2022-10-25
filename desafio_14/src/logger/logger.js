04e16dc8561c8f2c41b912dfc1dcdb85acad2a8c
DESKTOP-V2BVTP7\CMT <cristian_torchia@yahoo.com.ar>
2022-10-25 12:20:42 -0300

    Desafio 14: Avance en log de errores

--- desafio_14/src/logger/logger.js
+++ desafio_14/src/logger/logger.js
@@ -3,27 +3,32 @@ const log4 = require('log4js')
 log4.configure({
     appenders: {
         consola: { type: 'console' },
-        archivoErrores: { type: 'file', filename: 'logs/error.log' },
-        archivoDebug: { type: 'file', filename: 'logs/debug.log' },
+        archivoErrores: { type: 'file', filename: './logger/logs/error.log' },
+        archivoWarning: { type: 'file', filename: './logger/logs/warn.log' },
+        // archivoDebug: { type: 'file', filename: 'logs/debug.log' },
 
         loggerConsola: { type: 'logLevelFilter', appender: 'consola', level: 'info' },
         LoggerArchivoErrores: { type: 'logLevelFilter', appender: 'archivoErrores', level: 'error' },
-        LoggerArchivoDebug: { type: 'logLevelFilter', appender: 'archivoDebug', level: 'debug' }
+        LoggerArchivoWarning: { type: 'logLevelFilter', appender: 'archivoWarning', level: 'warn' }
+        // LoggerArchivoDebug: { type: 'logLevelFilter', appender: 'archivoDebug', level: 'debug' }
     },
     categories: {
         default: {
             appenders: ['loggerConsola'], level: 'all'
         },
         prod: {
-            appenders: ['LoggerArchivoErrores', 'LoggerArchivoDebug'], level: 'all'
+            // appenders: ['LoggerArchivoErrores', 'LoggerArchivoDebug'], level: 'all'
+            appenders: ['loggerConsola','LoggerArchivoErrores', 'LoggerArchivoWarning'], level: 'all'
         }
     }
 })
 
-let logger = null
-if (process.env.NODE_ENV === 'production') {
-    logger = log4.getLogger('prod')
-} else {
-    logger = log4.getLogger()    
-}
+logger = log4.getLogger('prod')
+
+// let logger = null
+// if (process.env.NODE_ENV === 'production') {
+//     logger = log4.getLogger('prod')
+// } else {
+//     logger = log4.getLogger()    
+// }
 module.exports = logger
