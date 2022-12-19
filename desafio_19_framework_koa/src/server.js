36e457e5ff013302ac6b452031df430e5552227d
DESKTOP-V2BVTP7\CMT <cristian_torchia@yahoo.com.ar>
2022-12-18 23:24:36 -0300

    Desafio 19: Minima limpieza

--- desafio_19_framework_koa/src/server.js
+++ desafio_19_framework_koa/src/server.js
@@ -21,10 +21,6 @@ const {koaBody} = require('koa-body')
 const app = new Koa()
 app.use(koaBody())
 
-// app.use(async ctx => {
-//     ctx.body = 'Hello Koa'
-// })
-
 app.use(routerProductos.routes())
 app.use(routerCarrito.routes())
 
