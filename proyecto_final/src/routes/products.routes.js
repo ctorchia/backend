c9151c1f717294ebbc7ca80c73589c0942c7cefc
DESKTOP-V2BVTP7\CMT <cristian_torchia@yahoo.com.ar>
2023-01-03 20:29:56 -0300

    Final: Preparando rutas de ordenes

--- proyecto_final/src/routes/products.routes.js
+++ proyecto_final/src/routes/products.routes.js
@@ -4,33 +4,25 @@ const {getProducts,getProductById,getProductByCategory,postProduct,putProduct,de
 const { Router } = express
 const routerProductos = Router()
 
- 
 //********************** GET (Devuelve todos los productos) **********************************
-
 routerProductos.get('/', getProducts)
 
 //********************** GET (Devuelve un producto según ID) **********************************
-
 routerProductos.get('/:id', getProductById)
 
 //********************** GET (Devuelve productos según Categoria) **********************************
-
 routerProductos.get('/categoria/:category', getProductByCategory)
 
 //************************ POST (Recibe y Agrega un producto) **********************************
-
 routerProductos.post('/', postProduct)
 
 //************************ PUT (Recibe y Actualiza un producto según su ID) ***********************
-
 routerProductos.put('/:id', putProduct)
 
 //************************ DELETE (Elimina un producto según su ID) ***********************
-
 routerProductos.delete('/:id', deleteProductById)
 
 //********************** '*' Rest of the routes **********************************
-
 routerProductos.get('*', routeNotAvailable)
 
 
